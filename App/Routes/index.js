87a95c4f3484eb80e70e6c271086da502aca8009
Dong Vu <vub1906614@student.ctu.edu.vn>
2023-02-15 01:15:34 +0700

    thuc hanh lab 1

---
+++ App/Routes/index.js
@@ -0,0 +1,8 @@
+import ContactRouter from './ContactRouter.js';
+import express from 'express';
+
+const Router = express.Router();
+
+Router.use('/contacts', ContactRouter);
+
+export default Router;
